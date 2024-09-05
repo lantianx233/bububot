04c64fd4d494e7c225cc3d395252fa59e77a2f99
lantianx <ltx233666777@outlook.com>
2024-09-05 23:39:16 +0800

    cmderr

--- src/bot.ts
+++ src/bot.ts
@@ -41,15 +41,19 @@ bot.command("capoo", async (ctx) => {
 
 
 
-bot.command("todo", async (ctx) => {
-    const messageText =
-        "//TODO 给bot搞个头像\n" +
-        "//TODO 搞个服务器部署\n" +
-        "大饼制作中...\n";
-    await ctx.reply(messageText,{
-        // `reply_parameters` 指定实际的回复哪一条信息。
-        reply_parameters: { message_id: ctx.msg.message_id },
-    })
+bot.command("capoo", async (ctx) => {
+    try {
+        const randomFilePath = await getRandomFile(); // 等待异步调用
+
+        await ctx.replyWithAnimation(new InputFile(randomFilePath), {
+            // `reply_parameters` 指定实际的回复哪一条信息。
+            reply_parameters: { message_id: ctx.msg.message_id },
+        });
+    } catch (error) {
+        // console.error('获取随机文件时出错:', error);
+        await ctx.reply(`咖波死了呜呜呜呜\n
+        死因：${error.message}\n`); // 错误处理
+    }
 });
 
 
