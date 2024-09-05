9fd485c86698b2fbab2c779dbefeaae6420facad
lantianx <128837704+lantianx233@users.noreply.github.com>
2024-09-06 02:28:56 +0800

    忙碌了一天的小丑主包发现Bot框架官方有托管教程 主包真的要发货了

--- src/webhook.ts
+++
@@ -1,9 +0,0 @@
-import { Bot } from "https://deno.land/x/grammy@v1.29.0/mod.ts";
-
-const bot = new Bot(Deno.env.get("7490814974:AAFERdZu-8CmQNmLMSagieAnbvuXgJ47AyA")!);
-
-
-
-// 设置 webhook
-const WEBHOOK_URL = "https://bububot.deno.dev/";
-await bot.api.setWebhook(WEBHOOK_URL);
