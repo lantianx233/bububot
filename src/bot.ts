25e1e48ead6c604ff2986489a42d66aef6b7b22d
lantianx <ltx233666777@outlook.com>
2024-09-05 16:00:19 +0800

    换成了deno

---
+++ src/bot.ts
@@ -0,0 +1,41 @@
+import { Bot , InputFile} from "https://deno.land/x/grammy@v1.29.0/mod.ts";
+import { autoRetry } from "https://deno.land/x/grammy_auto_retry@v2.0.2/mod.ts";
+import { getRandomFile } from './Capoo.ts';
+
+
+const bot = new Bot("7490814974:AAFERdZu-8CmQNmLMSagieAnbvuXgJ47AyA"); // <-- 把你的 bot token 放在 "" 之间
+
+bot.api.config.use(autoRetry());
+
+
+
+
+bot.command("start", (ctx) => ctx.reply("你好\n" +
+    "现在实现的功能：\n" +
+    "1.输入/capoo随机获取咖波，就一行代码（\n"));/*/start*/
+
+bot.command("capoo", async (ctx) => {
+    try {
+        const randomFilePath = await getRandomFile(); // 等待异步调用
+
+        await ctx.replyWithAnimation(new InputFile(randomFilePath), {
+            // `reply_parameters` 指定实际的回复哪一条信息。
+            reply_parameters: { message_id: ctx.msg.message_id },
+        });
+    } catch (error) {
+        // console.error('获取随机文件时出错:', error);
+        await ctx.reply('咖波死了呜呜呜呜\n'); // 错误处理
+    }
+});
+
+
+
+// bot.on("message", (ctx) => ctx.reply("Got another message!"));
+
+
+
+
+
+
+
+bot.start();
